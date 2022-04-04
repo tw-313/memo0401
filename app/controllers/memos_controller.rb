39ee08390005a2622c3f2b4bb363c7936aad33ab
tw-313 <tak7901y298@gmail.com>
2022-04-05 03:09:25 +0900

    削除、編集機能の追加

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -10,4 +10,20 @@ class MemosController < ApplicationController
     Memo.create(title:params["memos"]["title"],body:params["memos"]["body"])
     redirect_to "/"
   end
+
+  def destroy
+    memo = Memo.find(params["id"])
+    memo.destroy
+    redirect_to "/"
+  end
+
+  def edit
+    @memo = Memo.find(params["id"])
+  end
+
+  def update
+    memo = Memo.find(params["id"])
+    m.title = params["memos"]["title"]
+    m.body = params["memos"]["body"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to:'memos#index'
   get '/new', to:'memos#new'
   post '/create',  to:'memos#create'
+  delete "/memos/:id", to:"memos#destroy"
+  get "/memos/:id/edit", to:"memos#edit"
+  patch "/memos/:id", to:"memos#update"
 end
